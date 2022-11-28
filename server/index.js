acb75da156c6f31d5f9c6a38b006c06e101836fa
VNagorniy <vnagorniyrnd@gmail.com>
2022-11-28 16:52:14 +0300

    Создание папок клиент-сервер, с указанием сущностей на стороне сервера

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+app.use(express.json());
+
+// app.use('/api/auth', require('./routes/auth'));
+
+const PORT = config.get('port') || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoDB'));
+    app.listen(PORT, () => console.log(`App started on port ${PORT}`));
+  } catch (e) {
+    console.log('Server error', e.message);
+  }
+}
+
+start();
